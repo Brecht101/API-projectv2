6c4cef4110a832a546ab5adbd60a97edfe9d7b96
Brecht101 <r0878861@student.thomasmore.be>
2022-12-25 14:30:26 +0100

    changed put

--- app/crud.py
+++ app/crud.py
@@ -13,10 +13,15 @@ def get_user(db: Session, user_id: int):
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
-
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(first_name=user.first_name, last_name=user.last_name, password=user.password, registration_date=user.registration_date, id=user.id)
     db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def change_user(user: schemas.UserCreate, db: Session):
+    db_user = models.User(first_name=user.first_name, last_name=user.last_name, password=user.password, registration_date=user.registration_date, id=user.id)
     db.commit()
     db.refresh(db_user)
     return db_user
--- app/main.py
+++ app/main.py
@@ -59,9 +59,9 @@ def read_user(id: int = Query(default=None,gt=0,
     return db_user
 
 @app.put("/change")
-def change_user(id: int, user: schemas.UserCreate, db: Session = Depends(get_db)):
+def change_user(id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="Invalid ID or password!")
-    user.registration_date = datetime.datetime.now()
-    return crud.create_user(db=db, user=user)
+    db_user.registration_date = datetime.datetime.now()
+    return crud.change_user(db=db, user=db_user)
