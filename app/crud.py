2f69a2818a8a1498bf5c6a8d3e652cc433836e94
Brecht101 <r0878861@student.thomasmore.be>
2022-12-26 15:20:50 +0100

    added hashing

--- app/crud.py
+++ app/crud.py
@@ -2,9 +2,10 @@ import datetime
 
 from sqlalchemy.orm import Session
 
+
 import models
 import schemas
-
+import auth
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
@@ -14,7 +15,8 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(first_name=user.first_name, last_name=user.last_name, password=user.password, registration_date=user.registration_date, id=user.id)
+    hashed_password = auth.get_password_hash(user.password)
+    db_user = models.User(first_name=user.first_name, last_name=user.last_name, password=hashed_password, registration_date=user.registration_date, id=user.id)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
